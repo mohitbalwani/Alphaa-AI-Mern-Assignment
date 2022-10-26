e16f0773d6149b536ce36256722bed2d77621958
mohit <mohitbalwani5@gmail.com>
2022-10-26 19:33:26 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ReactEcharts from "echarts-for-react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// var myChart = echarts.init(chartDom);
+
+const option = {
+  title: {
+    text: 'Accumulated Waterfall Chart'
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    },
+    formatter: function (params) {
+      let tar;
+      if (params[1].value !== '-') {
+        tar = params[1];
+      } else {
+        tar = params[0];
+      }
+      return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+    }
+  },
+  legend: {
+    data: ['Loss', 'Profit']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: {
+    type: 'category',
+    data: (function () {
+      let list = [];
+      for (let i = 1; i <= 11; i++) {
+        list.push('Nov ' + i);
+      }
+      return list;
+    })()
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      name: 'Placeholder',
+      type: 'bar',
+      stack: 'Total',
+      itemStyle: {
+        borderColor: 'transparent',
+        color: 'transparent'
+      },
+      emphasis: {
+        itemStyle: {
+          borderColor: 'transparent',
+          color: 'transparent'
+        }
+      },
+      data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]
+    },
+    {
+      name: 'Profit',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'top'
+      },
+      data: [900, 345, 393, '-', '-', 135, 178, 286, '-', '-', '-']
+    },
+    {
+      name: 'Loss',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'bottom'
+      },
+      data: ['-', '-', '-', 108, 154, '-', '-', '-', 119, 361, 203]
+    }
+  ]
+};
+
+return <ReactEcharts option={option} />;
+
 }
+
 
 export default App;
